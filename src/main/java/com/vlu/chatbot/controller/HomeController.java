180e9f59cb7e841fd14517187ed9f4233c82f903
NTVinh04 <ngothanhvinh21104@gmail.com>
2025-11-29 01:11:40 +0700

    Add home

--- src/main/java/com/vlu/chatbot/controller/HomeController.java
+++ src/main/java/com/vlu/chatbot/controller/HomeController.java
@@ -1,16 +1,34 @@
 package com.vlu.chatbot.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
-    @GetMapping("/")
+    @GetMapping("/home")
     public String Home(){
         return "home";
     }
-    @GetMapping("/login")
+    @GetMapping("/")
     public String Login(){
         return "login";
     }
+    @GetMapping("/forgot-password")
+    public String Forgot(){
+        return "forgot";
+    }
+
+    @Controller
+    public class ChatController {
+            @GetMapping("/chat")
+            public String chatPage(Model model) {
+                String selectedUser = null; // hoặc tên user thực nếu đã chọn
+                model.addAttribute("selectedUser", selectedUser);
+                return "chat/home"; // dẫn tới home.html
+
+            }
+
+    }
+
 }
